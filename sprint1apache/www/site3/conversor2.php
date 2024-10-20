fb11708718c16764f0c7611130f6f85cdb76dca1
Chris <skm@Debian-DWServidor-PHP.myguest.virtualbox.org>
2024-10-20 21:47:53 +0200

    Ejercicio 6

--- sprint1apache/www/site3/conversor2.php
+++ sprint1apache/www/site3/conversor2.php
@@ -4,26 +4,43 @@
 <p>Convierte de la unidad especificada a metros</p>
 <p>
 <?php
-if (isset($_POST["funidad"])) {
-if ($_POST["funidad"] == "pulgada") {
-$v_pulgadas = $_POST["fcantidad"];
-$v_metros = $v_pulgadas * 0.0254;
-echo $v_pulgadas."pulgada(s) = ".$v_metros." metro(s)";
-} else {
-echo "Unidad no soportada";
-}
+if (isset($_POST["funidad"]) && isset($_POST["fcantidad"])) {
+    $cantidad = $_POST["fcantidad"];
+    $unidad = $_POST["funidad"];
+
+    // Validamos que la cantidad sea numérica
+    if (is_numeric($cantidad)) {
+        if ($unidad == "pulgada") {
+            $v_metros = $cantidad * 0.0254;
+            echo $cantidad . " pulgada(s) = " . $v_metros . " metro(s)";
+        } elseif ($unidad == "pie") {
+            $v_metros = $cantidad * 0.3048; // Conversión de pies a metros
+            echo $cantidad . " pie(s) = " . $v_metros . " metro(s)";
+        } else {
+            echo "Unidad no soportada.";
+        }
+    } else {
+        echo "Por favor, ingresa una cantidad válida.";
+    }
 }
 ?>
 </p>
 <p>Realiza una nueva conversión:</p>
 <form action="/conversor2.php" method="post">
-<label for="cantidad_input">Cantidad:</label><br>
-<input type="text" id="cantidad_input" name="fcantidad"><br>
-<input type="radio" id="pulgada_input" name="funidad" value="pulgada">
-<label for="pulgada_input">Pulgada(s)</label><br>
-<input type="radio" id="otro_input" name="funidad" value="otro">
-<label for="otro_input">Otro</label><br>
-<input type="submit" value="Convertir">
+    <label for="cantidad_input">Cantidad:</label><br>
+    <input type="text" id="cantidad_input" name="fcantidad" required><br>
+    
+    <input type="radio" id="pulgada_input" name="funidad" value="pulgada" required>
+    <label for="pulgada_input">Pulgada(s)</label><br>
+    
+    <input type="radio" id="pie_input" name="funidad" value="pie">
+    <label for="pie_input">Pie(s)</label><br>
+    
+    <input type="radio" id="otro_input" name="funidad" value="otro">
+    <label for="otro_input">Otro</label><br>
+    
+    <input type="submit" value="Convertir">
 </form>
 </body>
 </html>
+
