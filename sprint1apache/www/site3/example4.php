5499be557f686394a4bb1ed34e6785a001f1a0be
Chris <skm@Debian-DWServidor-PHP.myguest.virtualbox.org>
2024-10-21 09:01:50 +0200

    Tarea 4 arreglo

--- sprint1apache/www/site3/example4.php
+++ sprint1apache/www/site3/example4.php
@@ -1,44 +1,43 @@
 <html>
 <body>
-<h1> Jbilación</h1>
+<h1> Jubilación</h1>
 
-
-<?php>
+<?php
+$primo = 7;
 
 function edad_en_X_años($edad, $primo) {
-	return $edad  $primo;
+    // Suma la edad actual con el valor de $primo
+    return $edad + $primo;
 }
 
-
-function mensaje($age, $primo)
-	if(edad_en_X_años($age, $primo) >65) {
-		return "En $primo años tendrás edad de jubilación";
-	}else{
-		return "¡Disfruta de tu tiempo!";
-	}
-
+function mensaje($age, $primo) {
+    // Corrige la estructura y añade llaves
+    if(edad_en_X_años($age, $primo) > 65) {
+        return "En $primo años tendrás edad de jubilación";
+    } else {
+        return "¡Disfruta de tu tiempo!";
+    }
 }
 ?>
 
 <table border="1">
-<tr<
-<th>Edad</th>
-<th>Info</th>
-</tr>
+    <tr>
+        <th>Edad</th>
+        <th>Info</th>
+    </tr>
 
-<?php
+    <?php
+    $primo = 7;
+    $lista = array(53, 54, 55, 56, 57);
 
-$numero_primo = 7;
-$lista = array(53,54,55,56,57);
-
-foreach($lista as $valor){
-	echo "<tr>";
-	echo "<td>.$valor."</td>";
-	echo "<td>".mensaje($valor,$numero_primo)."</td>";
-	echo "</tr>";
-}
-?>
-
+    foreach($lista as $valor) {
+        echo "<tr>";
+        echo "<td>".$valor."</td>";
+        echo "<td>".mensaje($valor, $primo)."</td>";
+        echo "</tr>";
+    }
+    ?>
 </table>
 </body>
 </html>
+
